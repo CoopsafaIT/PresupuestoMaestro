ad2bf7b1b57550ec391d7f7a1a103bc60dc2dc3b
Juan Carlos Hernandez Reyes <juancalihernandez@gmail.com>
2022-03-04 13:58:59 -0600

    comenzando app de metas

---
+++ apps/goals/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    # path('', master_budget_dashboard, name="master_budget_dashboard"),
+]
--- ppto_safa/urls.py
+++ ppto_safa/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('indirect-budgets/', include('apps.indirect_budgets.urls')),
     path('income-budgets/', include('apps.income_budgets.urls')),
     path('cost-budgets/', include('apps.cost_budgets.urls')),
+    path('goals/', include('apps.goals.urls')),
     path('master-budget/', include('apps.master_budget.urls')),
     path(
         'master-budget/loan-portfolio/',
