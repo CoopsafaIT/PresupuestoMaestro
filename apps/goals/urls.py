7fa43512cd75f34bc32aea4f9c3db759302e346f
JuarezRoger <roger.juarez.rdjjm@gmail.com>
2022-03-16 23:18:35 -0600

    Reestructuracion de url y vistas

--- apps/goals/urls.py
+++ apps/goals/urls.py
@@ -1,23 +1,23 @@
 from django.urls import path
 from apps.goals.views import (
     goals_dashboard,
-    goals_for_period,
-    goals_period,
+    global_goals_period,
+    goals_period_edit,
     goals_global_definition,
     goals,
-    goal
+    goal_edit
 )
 
 
 urlpatterns = [
-    path('', goals_dashboard, name="goals_dashboard"),
-    path('goal-period-list/', goals_for_period, name="goals_for_period"),
-    path('goal-period-edit/<int:id>/', goals_period, name="goals_period_edit"),
+    path('goal/dashboard', goals_dashboard, name="goals_dashboard"),
+    path('global/period/', global_goals_period, name="global_goals_period"),
+    path('period/edit/<int:id>/', goals_period_edit, name="goals_period_edit"),
     path(
-        'goals-global-definition/<str:id_global_goal_period>/',
+        'global/definition/<str:id_global_goal_period>/',
         goals_global_definition,
         name="goals_global_definition"
     ),
-    path('goals-list/', goals, name="goals"),
-    path('goal/<int:id>/', goal, name="goal")
+    path('goals/list/', goals, name="goals"),
+    path('goal/edits/<int:id>/', goal_edit, name="goal_edit")
 ]
